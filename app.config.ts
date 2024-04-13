130b71c22297bc8ce3c2545de9b6268b727a9c9f
Denis Dmitriev <sbokyzahodiya@mail.ru>
2024-04-13 17:44:07 +1000

    clearup ui config

--- app.config.ts
+++ app.config.ts
@@ -15,10 +15,6 @@ export default defineAppConfig({
 
     },
 
-    notifications: {
-      position: 'bottom-0',
-    },
-
     modal: {
       container: 'items-center',
     },
@@ -34,32 +30,23 @@ export default defineAppConfig({
     pagination: {
       wrapper: 'flex items-center -space-x-px gap-2',
       base: 'rounded-md size-9 center',
-      rounded: 'first:rounded-s-md last:rounded-e-md',
       default: {
-        size: 'sm',
-        activeButton: {
-          color: 'primary',
-        },
         inactiveButton: {
           color: 'white',
         },
         firstButton: {
-          color: 'white',
           class: 'rtl:[&_span:first-child]:rotate-180 hidden',
           icon: 'i-heroicons-chevron-double-left-20-solid',
         },
         lastButton: {
-          color: 'white',
           class: 'rtl:[&_span:last-child]:rotate-180 hidden',
           icon: 'i-heroicons-chevron-double-right-20-solid',
         },
         prevButton: {
-          color: 'white',
           class: 'rtl:[&_span:first-child]:rotate-180',
           icon: 'i-heroicons-chevron-left-20-solid',
         },
         nextButton: {
-          color: 'white',
           class: 'rtl:[&_span:last-child]:rotate-180',
           icon: 'i-heroicons-chevron-right-20-solid',
         },
