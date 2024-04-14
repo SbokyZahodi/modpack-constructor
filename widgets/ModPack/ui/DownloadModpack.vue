1a73230bf39f5c43f291d812c718d2e1a88eaf1c
Denis Dmitriev <sbokyzahodiya@mail.ru>
2024-04-14 18:17:22 +1000

    DownloadState component

---
+++ widgets/ModPack/ui/DownloadModpack.vue
@@ -0,0 +1,15 @@
+<script lang='ts' setup>
+import { useDownloadModpack } from '../api'
+
+const { state, downloadModpack } = useDownloadModpack()
+</script>
+
+<template>
+  <UButton icon="material-symbols:download" variant="ghost" @click="downloadModpack">
+    Download
+  </UButton>
+
+  <UModal v-model="state.isFetching" prevent-close>
+    asd
+  </UModal>
+</template>
